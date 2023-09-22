393a219cda809076cebd493019950c748c8ffe5e
vhhgx <qieka_zhang@yeah.net>
2023-09-22 16:44:17 +0800

    init 创建模板

---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>该页面是默认布局页面</div>
+</template>
+
+<script>
+
+</script>
+
+<style></style>
---
+++ nuxt.config.js
@@ -0,0 +1,20 @@
+import {
+  resolve
+} from 'path'
+
+
+export default defineNuxtConfig({
+  devtools: {
+    enabled: true
+  },
+  alias: {
+    '@': resolve(__dirname, '/')
+  },
+  css: ['~/assets/styles/main.sass'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+})
